fbc26538365738f378663b270664e067c7215011
isaac <isaac.ingles@gmail.com>
2020-03-24 08:56:49 -0600

    added pages

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <h2>Changing some text</h2>
+      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+    </div>
+  )
+}
+
+export default HomePage
