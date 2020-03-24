c614e7a755015a8c28ed2143f6ff4c8d565acba5
isaac <isaac.ingles@gmail.com>
2020-03-24 09:23:58 -0600

    added layout and modified home and about pages

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout'
+
 const AboutPage = () => {
   return (
-    <div>
-      <h1>AboutPage</h1>
-      <p>Find me in ./web/src/pages/AboutPage/AboutPage.js</p>
-    </div>
+    <BlogLayout>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
+      <Link to={routes.home()}>Return home</Link>
+    </BlogLayout>
   )
 }
 
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,7 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
 const HomePage = () => {
-  return (
-    <div>
-      <h1>HomePage</h1>
-      <h2>Changing some text</h2>
-      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
-    </div>
-  )
+  return <BlogLayout>Home</BlogLayout>
 }
 
 export default HomePage
